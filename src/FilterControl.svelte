217f713fce26e9dbc84be961515cd15106cafc8a
Chris Mears <chris.mears@gmail.com>
2020-12-21 20:12:24 -0800

    feat: :sparkles: Add FilterControl component

---
+++ src/FilterControl.svelte
@@ -0,0 +1,40 @@
+<script lang="typescript">
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  export let options = [
+    { id: 'all', text: `All` },
+    { id: 'filter1', text: `Filter 1` },
+    { id: 'filter2', text: `Filter 2` },
+    { id: 'filter3', text: `Filter 3` },
+  ];
+  export let id = 'filter';
+  export let labelText = 'Filter';
+
+  let selected;
+
+  function handleChange() {
+    dispatch('updateFilter', {
+      value: selected.id,
+      text: selected.text,
+    });
+  }
+</script>
+
+<div class="filter-control">
+  <form on:change|preventDefault={handleChange}>
+    <div class="mt-2 mx-1 p-1 text-indigo-400 border-b border-indigo-100">
+      <label for={id}>{labelText}</label>
+      <select
+        bind:value={selected}
+        name={id}
+        {id}
+        class="w-full border-0 bg-transparent focus:ring-indigo-300 p-0 font-bold">
+        {#each options as option}
+          <option value={option}>{option.text}</option>
+        {/each}
+      </select>
+    </div>
+  </form>
+</div>
