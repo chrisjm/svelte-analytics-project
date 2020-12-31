6bc62ced6ca1380ef35241633906ffda7f567d75
Chris Mears <chris.mears@gmail.com>
2020-12-31 11:00:06 -0800

    feat: :sparkles: Update /logins_by_day endpoint
    Allow for use of filters based on app, org, and date range.

--- server/routes.ts
+++ server/routes.ts
@@ -1,5 +1,11 @@
 import type { FastifyInstance } from 'fastify';
 
+interface IQuerystring {
+  app: string;
+  org: string;
+  dateRange: string;
+}
+
 function processEvent(raw) {
   return {
     ...raw,
@@ -16,16 +22,48 @@ export default function (fastify: FastifyInstance, options, done) {
     };
   });
 
-  fastify.get('/logins_by_day', async (request, reply) => {
-    let logins = await fastify.db.all(`
+  // TODO: Initial options. Robustness needed.
+  const opts = {
+    schema: {
+      querystring: {
+        app: { type: 'string', enum: ['all', 'tm', 'omni', 'undefined'] },
+        org: { type: 'string' },
+        dateRange: { type: 'string' }
+      }
+    }
+  }
+
+  fastify.get<{
+    Querystring: IQuerystring
+  }>('/logins_by_day', opts, async (request, reply) => {
+    const { app, org, dateRange } = request.query;
+    let whereClause = '';
+
+    if (app !== 'all' && app !== 'undefined') {
+      whereClause += `WHERE application='${app}'`
+    }
+
+    if (org !== 'all' && org !== 'undefined') {
+      if (whereClause !== '') {
+        whereClause += ` AND `;
+      } else {
+        whereClause += `WHERE `;
+      }
+      whereClause += `org='${org}'`
+    }
+
+    const query = `
       WITH logins AS (
         SELECT DISTINCT date(timestamp, 'localtime') as day, user_id
         FROM sessions
+        ${whereClause}
       )
       SELECT day, count(*) as users
       FROM logins
       GROUP BY day
-    `);
+    `;
+
+    let logins = await fastify.db.all(query);
 
     return logins;
   });
@@ -33,13 +71,13 @@ export default function (fastify: FastifyInstance, options, done) {
   fastify.get('/logins_by_org', async (request, reply) => {
     let byOrg = await fastify.db.all(`
       WITH users AS (
-        SELECT DISTINCT user_id, orgs.name as org
+        SELECT DISTINCT user_id, orgs.name as org, orgs.id as org_id
         FROM sessions
         JOIN orgs on sessions.org=orgs.id
       )
-      SELECT org, count(*) as users
+      SELECT org_id, org, count(*) as users
       FROM users
-      GROUP BY org
+      GROUP BY org_id, org
     `);
 
     return byOrg;
